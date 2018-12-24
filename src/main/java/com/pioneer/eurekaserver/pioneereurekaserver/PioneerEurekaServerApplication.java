8415ba8c3a225dba9e8637e1c57f1d3d73acddcc
Ben Gonzales <bgonzales20001@gmail.com>
2018-12-24 16:04:08 -0600

    Init repo

---
+++ src/main/java/com/pioneer/eurekaserver/pioneereurekaserver/PioneerEurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.pioneer.eurekaserver.pioneereurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PioneerEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PioneerEurekaServerApplication.class, args);
+	}
+
+}
+
